588f1b5191ab771cdef93f60f0e6b0f11d7cbd08
Fakhry Fauzi <fakhry92@yahoo.com>
2019-09-13 22:38:17 -0400

    Added some variables into the class Field

--- Field.java
+++ Field.java
@@ -4,7 +4,13 @@ import java.awt.*;
 //add field to the java program
 public class Field extends JPanel{
 
+    int width  = 320;
+    int dot_size = 16;
+    int field_size = (320/16) * (320/16);
+    Image dot;
+    Image apple;
+
     public Field(){
-        
+        setBackground(Color.black);
     }
 }
--- Main.java
+++ Main.java
@@ -2,13 +2,14 @@ import javax.swing.*;
 
 public class Main extends JFrame{
     public static void main(String[] args) {
-        Main windonw = new Main();
+        Main window = new Main();
     }
     
     //Create a window object using the javax.swing plugins in java
     //This will create the game window
     
     public Main(){
+        add(new Field());
         setTitle("Java Snake Game");
         setLocation(300,500); //set the location within the screen
         setSize(320, 345); //set the size of the window
